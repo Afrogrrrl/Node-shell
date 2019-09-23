5c06e3b5875c1d2596cee146c539f4bce649ba66
Enya <enya.baez@gmail.com>
2019-09-23 12:00:24 -0400

    added pwd command

---
+++ bash.js
@@ -0,0 +1,16 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    process.stdout.write(process.cwd());
+  }
+  // process.stdout.write('you typed: ' + cmd);
+  // process.stdout.write('\nprompt > ');
+});
+
+
+// process.stdin.on('data', (data) => {
+//   const pwd = process.cwd();
+//   process.stdout.write(pwd);
+// })
